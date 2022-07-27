9502e9b5d5381b488f3dd436103d2bd07ab0343a
JohnQ1981 <ikram1981@gmail.com>
2022-07-27 18:23:02 -0500

    enhanced

--- Day002/tip_calculator.py
+++ Day002/tip_calculator.py
@@ -3,4 +3,7 @@ bill_paid = float(input("What was the total bill paid? "))
 tip_percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 how_many_people = int(input("How many people to split the bill? "))
 each_pays = ((bill_paid * tip_percentage / 100) + (bill_paid)) / how_many_people
+total = ((bill_paid * tip_percentage / 100) + (bill_paid))
+print(f"Total will including the tip percentage is: {round(total,2)}")
 print(f"Each Person should pay: ${round(each_pays,2)}")
+
