a7cab5dbf47099f5cd0ce72372df509d12e84bd9
JohnQ1981 <ikram1981@gmail.com>
2022-07-27 15:34:47 -0500

    more exercise

---
+++ Day001/day1.py
@@ -0,0 +1,30 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+#Write your code below this line 👇
+name = input("What is Your Name? ")
+print(len(name))
+# or other solution is:
+print( len ( input( "what is your name? ") ) )
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = b
+b = a
+a = c
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
