42f852cf1eb6bce59a163b02cd1020f06989f882
JohnQ1981 <ikram1981@gmail.com>
2022-07-28 15:26:01 -0500

    game

--- Day003/treasure_islan.py
+++ Day003/treasure_islan.py
@@ -38,23 +38,25 @@ print(art.text2art("Treasure", font='block'))
 print(art.text2art("ISLAND", font='block'))
 print("Your mission is to find the treasure")
 while True:
-    left_right = input("You are at a cross road, Where do you want to go? Type 'left' or 'right' \n")
+    left_right = input("You are at a cross road, Where do you want to go? Type 'left' or 'right' \n").lower()
     if left_right == "right" or left_right != 'left':
         print("You fell into a hole. Game Over!")
         break
     elif left_right == 'left':
-        swim_wait = input("You came to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. \n")
+        swim_wait = input("You came to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. \n").lower()
     if swim_wait == 'swim' or swim_wait != 'wait':
         print("You were attacked by trout. Game Over!")
         break
     elif swim_wait == 'wait':
-        select_color = input("You arrived at the Island unharmed. There is a house with 3 door. One red, one yellow and one blue. Which color do you choose? \n")
+        select_color = input("You arrived at the Island unharmed. There is a house with 3 door. One red, one yellow and one blue. Which color do you choose? \n").lower()
         if select_color == 'red':
             print("You are burned by fire. Game Over!")
+            break
         elif select_color == 'blue':
             print("You were eaten by beasts. Game Over!")
+            break
         elif select_color == 'yellow':
-            print("You won!")
+            print("You found the Treasure Box. You won!")
         else:
             print("You chose a door that doesn't exist. Game Over!")
             break
