36eb96cf6af1147390aefd8adead73f5e83330cb
JohnQ1981 <ikram1981@gmail.com>
2022-07-28 21:25:21 -0500

    Enhanced

--- Day004/nested_listV3.py
+++ Day004/nested_listV3.py
@@ -10,12 +10,19 @@ while True:
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
-    row = position[0]
-    column = position[1]
-    print(row,column)
-    column = int(column)
-    row = int(row)
-    map[column-1][row-1] = 'X'
+    
+
+    if position == '11' or position == '12' or position == '13' or position == '21' or position == '22' or position == '23' or position == '31' or position == '32' or position == '33' :
+        row = position[0]
+        column = position[1]
+        print(row,column)
+        column = int(column)
+        row = int(row)
+    
+    # if row > len(map[max][max]) and column > len(map[max][max]):
+    #     print("Out index range")
+    # else:
+        map[column-1][row-1] = 'X'
     # count =0
     # for c in map:
     #     if 'X' in c:
@@ -37,7 +44,10 @@ while True:
     #             row3 = ["⬜️","⬜️","⬜️"]
     #             map = [row1, row2, row3]
     #             print(f"{row1}\n{row2}\n{row3}")
-
+    elif position == "" or position == " ":
+        print("Try Again")
+    else :
+        print("Index out of range, Try again")
 
 
 #Write your code above this row 👆
