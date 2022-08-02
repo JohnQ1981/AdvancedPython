4df3cf322ff6b4b3375f38a86fc5f4630e3d61dd
JohnQ1981 <ikram1981@gmail.com>
2022-08-01 22:38:20 -0500

    Hang man

--- Day007/hang_man.py
+++ Day007/hang_man.py
@@ -14,4 +14,23 @@ print(art.text2art("Wlcm", font ='block'))
 words = ['Alican', 'Velican', 'Delican', 'Talican']
 ran = random.choice(words)
 print(ran)
-guess_word = input("Enter Letter of the Word Generated")
+kelime = []
+hesap = 0
+guess_word = input("Enter Letter of the Word Generated ")
+for c in ran:
+  if guess_word == ran:
+    kelime.append(guess_word)
+    print(f"{kelime} su ana kadar")
+  if kelime == ran:
+    print("You Won")
+  else:
+    guess_word = input("Enter Letter of the Word Generated ")
+  if guess_word != ran:
+    hesap += 1
+    if hesap == len(ran):
+      print("Game Over")
+    elif hesap < len(ran):
+      guess_word = input("Enter Letter of the Word Generated")
+    else:
+      print('You Won!')
+
