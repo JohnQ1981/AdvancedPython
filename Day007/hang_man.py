2a70112981ee156623321f685dcd7421229d4ff0
JohnQ1981 <ikram1981@gmail.com>
2022-08-02 21:29:35 -0500

    hang man

--- Day007/hang_man.py
+++ Day007/hang_man.py
@@ -11,26 +11,35 @@ print('''_____
       _____,-\__..__|_____Pr59''')
 print(art.text2art("Hello", font='block'))
 print(art.text2art("Wlcm", font ='block'))
-words = ['Alican', 'Velican', 'Delican', 'Talican']
+words = ['Alican', 'Velican', 'Delican', 'Talican', 'Hilola','Abdurrahmon']
 ran = random.choice(words)
 print(ran)
-kelime = []
-hesap = 0
-guess_word = input("Enter Letter of the Word Generated ")
+kelime =''
+#kelime = ran.replace("", "*")
 for c in ran:
-  if guess_word == ran:
-    kelime.append(guess_word)
-    print(f"{kelime} su ana kadar")
-  if kelime == ran:
-    print("You Won")
-  else:
-    guess_word = input("Enter Letter of the Word Generated ")
-  if guess_word != ran:
-    hesap += 1
-    if hesap == len(ran):
-      print("Game Over")
-    elif hesap < len(ran):
-      guess_word = input("Enter Letter of the Word Generated")
-    else:
-      print('You Won!')
+  kelime += "_"
+print(kelime, len(kelime))
+# word_blank = str(len(kelime))
+# print(word_blank)
+# print(kelime)
+# hesap = 0
+guess_word = input("Enter Letter of the Word Generated1 ")
+if guess_word in ran: 
+  print(f"{kelime} su ana kadar {ran[ran.find(guess_word)]} and index is {ran.find(guess_word)}")
+  kelime = kelime[:ran.find(guess_word)] + guess_word + kelime[ran.find(guess_word)+1:]
+  #kelime[int(ran.find(guess_word))] = guess_word
+  #kelime = kelime.replace(int(ran.find(guess_word)),ran[ran.find(guess_word)])
+print(f"{kelime}")
+# if kelime == ran:
+#   print("You Won")
+# else:
+#   guess_word = input("Enter Letter of the Word Generated2 ")
+# if guess_word != ran:
+#   hesap += 1
+# if hesap == len(ran):
+#     print("Game Over")
+# elif hesap < len(ran):
+#     guess_word = input("Enter Letter of the Word Generated3")
+# else:
+#     print('You Won!')
 
