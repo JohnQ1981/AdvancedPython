65e16b7bae1f45447364958673f2a4de06f30c1f
JohnQ1981 <ikram1981@gmail.com>
2022-08-03 20:59:11 -0500

    s

--- Day007/hang_man_my.py
+++ Day007/hang_man_my.py
@@ -11,7 +11,7 @@ print('''_____
       _____,-\__..__|_____Pr59''')
 print(art.text2art("Hello", font='block'))
 print(art.text2art("Wlcm", font ='block'))
-words = ['alican', 'velicana', 'delicana', 'talican', 'hilola','abdurrahmon']
+words = ['alican', 'velica', 'delica', 'talica', 'hilola','salih']
 ran = random.choice(words)
 print(ran, len(ran))
 display = []
@@ -19,18 +19,89 @@ for c in ran:
   display += "_"
 print(display)
 countb = 0
-while countb < len(ran):
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+end_game = False
+lives = 6
+while not end_game:
   guess_word = input("Enter Letter of the Word Generated1 ")
   countb += 1
+  
   for position in range(len(ran)):
     letter = ran[position]
     if letter == guess_word:
       display[position] = letter
+    
+
+  if guess_word not in ran:
+      lives -=1
+      print(f"{stages[lives]}")
+      if lives == 0 :
+        end_game = True
+        print("You Lost")
+
   print(display)
-  print(f"Count is :{countb}")
+  print(f"Count is :{countb}, be careful and you have only {len(ran)-countb+1} guess(s) left. ")
   guess = list(ran)
-  if display == guess:
+  if display == guess or "_" not in display:
     print("You Won")
     break
-  elif display != guess and countb == len(ran):
-    print("You lost")  
+  elif display != guess and lives == 0:
+    print("You lost")
+    break
+
+          
