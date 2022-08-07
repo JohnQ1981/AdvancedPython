7b10e1cfbe57886f0372fb2e6db3b2067b40b9a1
JohnQ1981 <ikram1981@gmail.com>
2022-08-07 00:23:49 -0500

    auction done

--- Day009/auction.py
+++ Day009/auction.py
@@ -6,8 +6,16 @@ print("Welcome to the secret auction program.")
 lp = True
 bidders = {}
 actual_bidders = []
-max_bid = 0
-#count = 0
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  # bidding_record = {"Angela": 123, "James": 321}
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid: 
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
 while lp:
     user_name = input("What is your name?: ")
     bid_amount = int(input("What is your bid?: $ "))    
@@ -20,12 +28,13 @@ while lp:
     if more_bidders == 'yes':
         clear()
     elif more_bidders == 'no':
-        for bids, values in bidders.items():
-            if values > max_bid:
-                max_bid = values
-        print(f"The winner is: {max_bid} and name is: {bidders.keys()} ")
+        # for bids, values in bidders.items():
+        #     if values > max_bid:
+        #         max_bid = values
+        # print(f"The winner is: {max_bid} and name is: {bidders.keys()} ")
         lp = False
-    else:
-        print("incorrect input,try again!")
+        find_highest_bidder(bidders)
+    # else:
+    #     print("incorrect input,try again!")
 
     print(bidders)
